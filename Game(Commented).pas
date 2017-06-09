4a33d4bb35c851111703320cc83d0d870d31d305
Malez228 <andrey193728@yandex.ru>
2017-06-09 12:06:42 +0300

    Вроде все что нужно пофиксил
    А так же вынес коменты в отдельный файл, чтобы не мешали

--- Game(Commented).pas
+++ Game(Commented).pas
@@ -5,7 +5,9 @@ interface
 uses System, System.Drawing, System.Windows.Forms,kk;
 
 var
+  //Время игры и цифра, которую нужно нажать в данный момент
   time, c: integer;
+  //Без коментариев -_-
   maxbutton := Wid * Hei;
 
 type
@@ -18,7 +20,7 @@ type
     procedure timer2_Tick(sender: Object; e: EventArgs);
     procedure timer3_Tick(sender: Object; e: EventArgs);
   {$region FormDesigner}
-  private 
+  private
     {$resource Game.Form12.resources}
     timer1: Timer;
     button1: Button;
@@ -39,25 +41,28 @@ implementation
 
 procedure Form12.Form1_Load(sender: Object; e: EventArgs);
 var
-  margin := 12;
-  cellSize := 70;
-  indent := 6;
+  margin := 12;//Отступ от края
+  cellSize := 70;//Размер ячейки
+  indent := 6;//Отступ между ячейками
 begin
-  if Wid <= 1 then Wid := 2;
-  if Hei <= 1 then Hei := 2;
+  if Wid <= 1 then Wid:=2;
+  if Hei <= 1 then Hei:=2;
   maxbutton := Wid * Hei;
   
+  //Интервал таймера подсказок
   Timer2.Interval := Delay * 1000 + 1;
   
+  //Подстраиваем размер окна под выбранный размер поля
   self.Width := margin * 2 + cellSize * Wid + indent * (Wid + 1) + indent div 2;
   self.Height := margin * 3 + cellSize * (Hei + 1) + indent * (Hei + 3) + indent;
   self.MinimumSize := Self.Size;
   self.maximumSize := Self.Size;
   
+  //Создаем кнопки в RealTime
   for var i := 0 to Wid - 1 do
     for var j := 0 to Hei - 1 do
     begin
-      var b := new Button();
+      var b:= new Button();
       b.Name := 'But' + i;
       b.Width := cellSize;
       b.Height := cellSize;
@@ -67,6 +72,7 @@ begin
       Controls.Add(b);
     end;
   
+  //Кнопку начать просто изменим, иначе обработчик не будет связан с формой
   button1.Width := self.Width - Margin * 3 - indent div 2;
   button1.Height := cellSize;
   button1.Location := new Point(margin, self.Height - Margin - cellSize - cellSize div 2);
@@ -74,6 +80,7 @@ end;
 
 procedure Form12.timer1_Tick(sender: Object; e: EventArgs);
 begin
+  //Каждый тик увеличиваем время
   inc(time);
   button1.Text := 'Секунд прошло: ' + time;
 end;
@@ -83,80 +90,117 @@ var
   a: List<integer>;
 begin
   a := new List<integer>;
+  //Если мы работаем для кнопки начать, то
   if isBegin then
   begin
+    //Нам плевать и мы хреначим массив сначала
     for var i := 1 to maxbutton do
       a.Add(i);
   end else
   begin
+    //Иначе (когда мы работаем для перемешивания) мы берем цифры с активных кнопок
     foreach var con: Control in f.Controls do
-      if (con.Name <> 'button1') and (con.Name <> 'button2') and (con.Enabled) then
-        a.Add(strtoint(con.Text));
+      if (con.Name <> 'button1') and (con.Name <> 'button2') then
+        if con.Enabled then
+          a.Add(strtoint(con.Text));
   end;
   
+  //Бегаем по кнопкам
   foreach var con: Control in f.Controls do
     if con.Enabled then
       if (con.Name <> 'button1') and (con.Name <> 'button2') then
       begin
-        var rnd: integer;
-        if a.Count - 1 <= 0 then rnd := 0
+        //Рандомим циферку
+        var rnd:integer;
+        if a.Count-1 <= 0 then rnd := 0
         else rnd := PABCSystem.Random(0, a.Count - 1);
+        //Делаем штуки с кнопкой
         con.Text := a[rnd] + '';
         con.ForeColor := Color.FromArgb(255, PABCSystem.Random(255), PABCSystem.Random(255), PABCSystem.Random(255));
         con.BackColor := Color.FromArgb(255, 225, 225, 225);
+        //И удаляем нашу цифру из массива, чтобы больше ее не видеть
         a.RemoveAt(rnd);
       end;
 end;
 
+//Кнопка начать
 procedure Form12.button1_Click(sender: Object; e: EventArgs);
 begin
+  //Создаем массив с цифрами для кнопок и заливаем их туда
   var a: List<integer>;
   a := new List<integer>;
   for var i := 1 to maxbutton do
     a.Add(i);
   
+  //Обновляем переменные всякие
   c := 1;
   time := 0;
   timer1.Enabled := true;
-  if kk.Help then timer2.Enabled := true;
+  if kk.Help then timer2.Enabled := true;  
+  //А вот и та самая проверочка, которую нужно было раньше(зачеркнуто) ниже пихнуть
+  //(Это таймер для автозаполнения)
   if Aut then timer3.Enabled := true;
   
+  //Бегаем по кнопкам
   foreach var con: Control in self.Controls do
     if (con.Name <> 'button1') and (con.Name <> 'button2') then
+      //Их нужно включить, иначе в последующих играх они будут неактивны
       con.Enabled := true;
   
+  //Рисуем случайные цифры на кнопках
   NumGen(self, true);
+  //Кнопочку после этого конечно выключим
   button1.Enabled := false;
 end;
 
 procedure Form12.Form12_FormClosed(sender: Object; e: FormClosedEventArgs):=
 Application.Exit();
 
+//Обработчик создаваемых кнопок
 procedure Form12.button2_Click(sender: Object; e: EventArgs);
 begin
+  //Включен таймер - игра идет
   if timer1.Enabled then
   begin
+    //Включаем подсказки, если нужно
     if kk.Help then timer2.Enabled := false;
     
+    //Получаем кнопку, которая начала этот ад
     var b: Button;
     b := (sender) as Button;
+    //Если ее содержимое равно нужной нам цифре, то
     if c = strtoint(b.Text) then
-    begin
-      b.Enabled := false;
-      inc(c);
-      b.BackColor := Color.FromArgb(255, 250, 210, 1);
-      if Mix then NumGen(self, false);
-    end;
+      //Если цифры не нужно перемешивать, то
+      if not Mix then
+      begin
+        //Просто выключаем кнопку, увеличиваем нужную цифру
+        b.Enabled := false;
+        inc(c);
+        if kk.Help then timer2.Enabled := true;
+        //ну и можно цвет поменять, чтоб заметнее было
+        b.BackColor := Color.FromArgb(255, 250, 210, 1);
+      end else
+      begin
+        //Если же нам нужно перемешивать, то делаем то же самое
+        b.Enabled := false;
+        inc(c);      
+        b.BackColor := Color.FromArgb(255, 250, 210, 1);
+        //Но передаем 'false' т.к. мы перемешиваем
+        NumGen(self, false);
+      end;
     if kk.Help then timer2.Enabled := true;
     
+    //Проверка на победку
     if c >= maxbutton + 1 then 
     begin
       timer1.Enabled := false;
       MessageBox.Show('Победа' + #10 + 'Секунд прошло: ' + time);
       
+      //По сути тут должно буть условие но всем плевать
       timer3.Enabled := false;
       timer2.Enabled := false;
       
+      //Записываем наши достижения в файлик
       var localDate := DateTime.Now;
       var rec: PABCSystem.Text;
       Assign(rec, 'Records.txt');
@@ -170,26 +214,32 @@ begin
         Writeln(rec, localDate + ' ' + time + 'сек.' + ' поле ' + Wid + 'x' + Hei + ' режим перемешивания включен');
       PABCSystem.Close(rec);
       
+      //Ну и вернем кнопку, чтобы можно было еще поиграть
       button1.Text := 'Начать';    
       button1.Enabled := true;
     end;
   end;
 end;
 
+//Наши подсказки
 procedure Form12.timer2_Tick(sender: Object; e: EventArgs);
 begin
+  //Все предельно просто
   foreach var con: Control in self.Controls do
     if con.Text = inttostr(c) then
     begin
+      //Заменил цвет и выключился
       con.BackColor := Color.FromArgb(255, 113, 206, 235);
       timer2.Enabled := false;
     end;
 end;
 
+//Лучший бот для автозаполнения...
 procedure Form12.timer3_Tick(sender: Object; e: EventArgs);
 begin
   if timer1.Enabled then
     foreach var con: Control in self.Controls do
+      //Да, он просто проверяет текст и 'жмет' на кнопку
       if con.Text = inttostr(c) then
         button2_click(con, new EventArgs);
 end;
